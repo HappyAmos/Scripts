4b0b570ad0aac8c85da55d88fdd5ad122285bb4b
andrew <adiamond1978@gmail.com>
2025-04-05 17:04:04 -0400

    test

--- setupha.sh
+++ setupha.sh
@@ -95,7 +95,8 @@ install_cheat_glow() {
 	gunzip cheat-linux-"$arch".gz
 	chmod +x cheat-linux-"$arch"
 	"$SUDO" mv cheat-linux-"$arch" /usr/local/bin/cheat
-	mkdir -p ~/.config/cheat && cheat --init > ~/.config/cheat/conf.yml
+
+	cheat
 
 	cd || exit
 
